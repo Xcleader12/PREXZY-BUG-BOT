92196f2d7a908cc9e22dbc23fb3be2932fd59e7f
Xcleader12 <xcleader6@gmail.com>
2024-09-09 18:37:15 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "263717750825"
-global.ownername = "Precious Ayomide👑"
+global.ownernomer = "2347088481182"
+global.ownername = "XC LEADER"
 global.ytname = "YT: Prexzyvilla"
 global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
